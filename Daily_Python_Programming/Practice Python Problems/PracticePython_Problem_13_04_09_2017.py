134b0945b696350248ef9044bbd3c34510f277a7
Rohit Gupta <ezi.leo@gmail.com>
2017-12-11 13:12:47 +0530

    Daily Programming commits

---
+++ Daily_Python_Programming/Practice Python Problems/PracticePython_Problem_13_04_09_2017.py
@@ -0,0 +1,51 @@
+# URL: http://www.practicepython.org/
+# Author: Rohit Gupta
+# Date Scheduled: September 01, 2017
+# Date Written: October 26, 2017
+# Version 2.4
+# Problem Exercise: 13
+
+import datetime
+import random
+
+
+def main():
+    script_header()
+    user_input = int(input("Enter 1 to choose FS or 2 to Randomly generate one: "))
+    if user_input == 1:
+        fs_length = int(input("Enter how long you want to calculate this: "))
+        fs(fs_length)
+    elif user_input == 2:
+        fs_length = random.randint(1, 100)
+        fs(fs_length)
+    else:
+        print("Please select valid input")
+
+
+def script_header():
+    print("Welcome to the programme to calculate the fibonacci series")
+    user_name = input("Please enter your name: ")
+    print("The programme was run on: ", datetime.datetime.now())
+    print("Your interaction with the script starts {} . . . . .".format(user_name))
+    return
+
+
+def fs(fs_length):
+    if fs_length == 1:
+        fs0 = [0]
+        print(fs0)
+    elif fs_length == 2:
+        fs0 = [0, 1]
+        print(fs0)
+    elif fs_length > 2:
+        fs0 = [0, 1]
+        a = 1
+        while a <= (fs_length-2):
+            fs0.append(fs0[a] + fs0[a-1])
+            a += 1
+    print(fs0)
+    print(len(fs0))
+
+
+if __name__ == '__main__':
+    main()
--- Daily_Python_Programming/Practice Python Problems/PracticePython_Problem_14_05_09_2017.py
+++ Daily_Python_Programming/Practice Python Problems/PracticePython_Problem_14_05_09_2017.py
@@ -2,10 +2,13 @@
 # Author: Rohit Gupta
 # Date Scheduled: September 01, 2017
 # Date Written: November 29, 2017
-# Version 1.0
+# Version 1.3
 # Problem Exercise: 14
 import datetime
 import random
+
+list1 = []
+list2 = []
 
 
 def main():
@@ -14,8 +17,8 @@ def main():
 
 
 def header():
-    print("Welcome to the Programme to de duplicate lists")
-    user_name = input("PLease enter your name")
+    print("= = = = Welcome to the Programme to de duplicate lists = = = = ")
+    user_name = input("PLease enter your name: ")
     print("The programme was run on", datetime.datetime.now())
     print("Your interaction with the script starts {}". format(user_name))
 
@@ -24,8 +27,7 @@ def define_lists():
     user_input = int(input("Please enter 1 to enter a list or "
                            "2 to generate random lists: "))
     if user_input == 1:
-        list1 = []
-        list2 = []
+
         a = 1
         while a <= 10:
             digit1 = int(input("Enter {} digit to be part of First list".format(a)))
@@ -35,22 +37,27 @@ def define_lists():
             a += 1
         print("The generated lists are: ", list1, list2)
         print("The length of list 1 is: ", len(list1), "and of second list is:", len(list2))
-        de_dupe(list1, list2)
+        de_dupe()
+
     elif user_input == 2:
         a1 = random.randint(10, 20)
         a2 = random.randint(10, 20)
-        rlist1 = []
-        rlist2 = []
+
         for _ in range(0, a1):
-            rlist1.append(random.randint(0, 1000))
+            list1.append(random.randint(0, 1000))
         for _ in range(0, a2):
-            rlist2.append(random.randint(100, 1000))
-        print("Random List 1:", rlist1, "Length of list is: ", len(rlist1))
-        print("Random List 1:", rlist2, "Length of list is: ", len(rlist2))
+            list2.append(random.randint(100, 1000))
+        print("Random List 1:", list1, "Length of list is: ", len(list1))
+        print("Random List 2:", list2, "Length of list is: ", len(list2))
+        de_dupe()
 
 
 def de_dupe():
     print(list1, list2)
+    final_list = list1 + list2
+    print(sorted(final_list), "the length is {}".format(len(final_list)))
+    dedupe_list = set(final_list)
+    print(sorted(dedupe_list), "type is: {} and length is {}".format(type(dedupe_list), len(dedupe_list)))
 
 
 if __name__ == "__main__":
